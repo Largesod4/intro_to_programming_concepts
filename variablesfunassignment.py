133ce4cd59a5d120d520f26cbcf8923ce5104251
Largesod4 <martinacevedo34@hotmail.com>
2026-01-25 15:23:37 -0500

    variables assignment

---
+++ variablesfunassignment.py
@@ -0,0 +1,12 @@
+first = 5 * 6
+print("the answer to your query is", first)
+second = 8 / 3
+print("the answer to your query is", second)
+third = 8 // 3
+print("the answer to your query is", third)
+fourth = 8 % 3
+print("the answer to your query is", fourth)
+fifth = 4200000 + 23090
+print("the answer to your query is", fifth)
+sixth = 4200000 - 7633
+print("the answer to your query is", sixth)
